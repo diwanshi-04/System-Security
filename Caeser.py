8f6f40a4095912f2b3d425167d50a92329fca574
diwanshi-04 <diwanshipandey331@gmail.com>
2025-08-16 23:53:41 +0530

    Create Caeser.py

---
+++ Caeser.py
@@ -0,0 +1,27 @@
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# Encryption
+plain_text = input("Plain Text: ")
+cipher_text = ''
+k = 3
+for char in plain_text:
+    if char.upper() in alpha:
+        p_idx = alpha.index(char.upper())
+        ct_idx = (p_idx + k)%26
+        cipher_text+=alpha[ct_idx]
+    else:
+        cipher_text+=char
+    
+print("Cipher Text:", cipher_text)
+
+# Decryption
+plain_text = ''
+for char in cipher_text:
+    if char.upper() in alpha:
+        ct_idx = alpha.index(char.upper())
+        p_idx = (ct_idx - k)%26
+        plain_text+=alpha[p_idx]
+    else:
+        plain_text+=char
+
+print("Dechiphered Text:", plain_text
